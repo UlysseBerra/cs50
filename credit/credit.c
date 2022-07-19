517af477d388908480f5f66e552515c5a9851ec1
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-19 21:30:36 +0000

    Tue, Jul 19, 2022, 9:30 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,11 +7,10 @@ int main(void)
 {
     long n = get_long("Number: ");
     isInvalid(n);
-
 }
 
 bool isInvalid(long n)
 {
-    printf("%li\n", n%100);
+    printf("%li\n", (n%100)-((n%100)%10));
     return true;
 }
