cc966cb55f5e94ac0d9d439711178aad9479230e
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-19 21:13:57 +0000

    Tue, Jul 19, 2022, 9:13 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,14 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-string card_type(long n)
-
 int main(void)
 {
     long n = get_long("Number: ");
-}
 
-string card_type(long n)
-{
-    
+    n = (string) n;
+    if (n[1] == 4)
+    {
+        printf("e");
+    }
 }
