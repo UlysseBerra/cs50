ab19b4a066fbd422e98a5892902f3d65a273c4df
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-19 21:32:38 +0000

    Tue, Jul 19, 2022, 9:32 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,16 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
-bool isInvalid(long n);
-
 int main(void)
 {
     long n = get_long("Number: ");
     isInvalid(n);
-}
-
-bool isInvalid(long n)
-{
-    printf("%li\n", (n%100)-((n%100)%10));
-    return true;
 }
