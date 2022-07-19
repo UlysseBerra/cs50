9117dad757dc1d5262cf707336cbbc8c1de893da
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-19 22:04:59 +0000

    Tue, Jul 19, 2022, 10:04 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool luhn(long n);
+
 int main(void)
 {
     long n = get_long("Number: ");
+
+    if (luhn(n) == false)
+    {
+        printf("INVALID\n");
+    }
 }
+
+bool luhn(long n)
+{
+    
+    bool result;
+    return result;
+}
+
