ec3ae3832d640d703ca3d1389109e5f4d892be2c
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-19 21:21:42 +0000

    Tue, Jul 19, 2022, 9:21 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -4,6 +4,13 @@
 int main(void)
 {
     long n = get_long("Number: ");
+    double first_digit = n;
 
-    
+    while (first_digit >= 10)
+    {
+        first_digit = n / 10;
+    }
+
+    printf("%li", n);
+    printf("%f", first_digit);
 }
