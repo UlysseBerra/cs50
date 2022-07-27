75442f5b7eee342a79e493996324893eff06bbda
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-27 15:51:21 +0000

    Wed, Jul 27, 2022, 3:51 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 // Points assigned to each letter of the alphabet
-int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
 
@@ -24,4 +24,5 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    return 0;
 }
