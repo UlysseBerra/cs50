f836b22443c947a54a247fa36711921fdcbab028
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-27 15:54:10 +0000

    Wed, Jul 27, 2022, 3:54 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,11 +18,18 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else
+    {
+        printf("Player 2 wins!");
+    }
 }
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    
     return 0;
 }
