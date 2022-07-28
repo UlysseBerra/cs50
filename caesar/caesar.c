f49b101eb2e5536e9af8a6898491c2748e0ede3c
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-28 09:26:34 +0000

    Thu, Jul 28, 2022, 9:26 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
-int main(int argc, string argv[]) 
+string encrypt(string plaintext);
+
+int main(int argc, string argv[])
 {
 
+}
+
+string encrypt(string plaintext)
+{
+    
 }
