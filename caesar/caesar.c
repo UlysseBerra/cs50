8ba6206c17c13e248a9a05ebe4bb220e63659c67
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-28 09:58:46 +0000

    Thu, Jul 28, 2022, 9:58 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,7 +10,7 @@ int main(int argc, string argv[])
 {
     if (argc != 2 || atoi(argv[1]) <= 0)
     {
-        printf("Usage: ./caesar key\n");
+        printf("Usage: ./caesar key");
         return 1;
     }
 
