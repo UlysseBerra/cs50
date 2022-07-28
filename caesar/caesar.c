a7d11284a8bddbdb6078e154b898e68e89c4bea2
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-07-28 09:30:35 +0000

    Thu, Jul 28, 2022, 9:30 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,15 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 string encrypt(string plaintext);
 
 int main(int argc, string argv[])
 {
-
+    if (!argv[1] || )
+    {
+        printf("Usage: ./caesar [key]")
+    }
+    int key = (int) argv[1];
 }
 
-string encrypt(string plaintext)
+string encrypt(string plaintext, int key)
 {
-    
+
 }
