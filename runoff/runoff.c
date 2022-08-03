373e275e1e0052e7c362e4dca2262d180d7a7422
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-08-03 16:11:37 +0000

    Wed, Aug 3, 2022, 4:11 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,4 +1,36 @@
-#include "runoff.h"
+#include <cs50.h>
+#include <stdio.h>
+
+// Max voters and candidates
+#define MAX_VOTERS 100
+#define MAX_CANDIDATES 9
+
+// preferences[i][j] is jth preference for voter i
+int preferences[MAX_VOTERS][MAX_CANDIDATES];
+
+// Candidates have name, vote count, eliminated status
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX_CANDIDATES];
+
+// Numbers of voters and candidates
+int voter_count;
+int candidate_count;
+
+// Function prototypes
+bool vote(int voter, int rank, string name);
+void tabulate(void);
+bool print_winner(void);
+int find_min(void);
+bool is_tie(int min);
+void eliminate(int min);
 
 int main(int argc, string argv[])
 {
