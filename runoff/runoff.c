de16057bc7bb09ece808353fdb84f922654880ef
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-10-08 11:01:32 +0000

    ddd, Oct 10/8/2022, YYYY, 11:01 A +0

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -14,8 +15,7 @@ typedef struct
     string name;
     int votes;
     bool eliminated;
-}
-candidate;
+} candidate;
 
 // Array of candidates
 candidate candidates[MAX_CANDIDATES];
@@ -127,41 +127,112 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    // If name is a match for the name of a valid candidate, then you should update the global preferences array to indicate that the voter voter has that candidate as their rank preference (where 0 is the first preference, 1 is the second preference, etc.)
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!strcmp(candidates[i].name, name))
+        {
+            preferences[voter][rank] = i;
+            // If the preference is successfully recorded, the function should return true
+            return true;
+        }
+        // The function returns false otherwise (if, for instance, name is not the name of one of the candidates)
+    }
     return false;
 }
 
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
-    return;
+    // The function should update the number of votes each candidate has at this stage in the runoff
+    for (int i = 0; i < voter_count; i++)
+    {
+        int j = 0;
+        // Search for remaining candidates
+        while (candidates[preferences[i][j]].eliminated)
+        {
+            j++;
+        }
+        // Update their votes
+        candidates[preferences[i][j]].votes++;
+    }
 }
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
-    return false;
+    // If any candidate has more than half of the vote, their name should be printed and the function should return true.
+    int winner_exists = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated && candidates[i].votes > (voter_count / 2))
+        {
+            winner_exists++;
+            printf("%s\n", candidates[i].name);
+        }
+    }
+    if (winner_exists)
+    {
+        return true;
+    }
+    else
+    {
+        // If nobody has won the election yet
+        return false;
+    }
 }
 
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    // The function should return the minimum vote total for any candidate who is still in the election.
+    int min = voter_count;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated && candidates[i].votes <= min)
+        {
+            min = candidates[i].votes;
+        }
+    }
+    return min;
 }
-
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    return false;
+    int remaining = 0;
+    int having_min = 0;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated)
+        {
+            remaining++;
+            if (candidates[i].votes == min)
+            {
+                having_min++;
+            }
+        }
+    }
+    if (remaining == having_min)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 // Eliminate the candidate (or candidates) in last place
+// The function takes an argument min, which will be the minimum number of votes that anyone in the election currently has.
 void eliminate(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // The function should eliminate the candidate (or candidates) who have min number of votes.
+        if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
     return;
 }
