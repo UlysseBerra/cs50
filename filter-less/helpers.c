1c54e1c1a602b79c76cae6535f0e93a3fb9900f8
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 10:45:26 +0000

    Thu, Nov 3, 2022, 10:45 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -20,6 +20,19 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            double sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+            double sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+            double sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+
+            image[i][j].rgbtRed = avg_color;
+            image[i][j].rgbtGreen = avg_color;
+            image[i][j].rgbtBlue = avg_color;
+        }
+    }
     return;
 }
 
