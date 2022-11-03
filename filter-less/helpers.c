19b436b46648242fc94483fecdc6107f0d8fb99c
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 15:26:16 +0000

    Thu, Nov 3, 2022, 3:26 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -36,10 +36,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // calculate colors
-            double sepiaRed = (int)round(0.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
-            double sepiaGreen = (int)round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
-            double sepiaBlue = (int)round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen
-                                          + .131 * image[i][j].rgbtBlue);
+            double sepiaRed = (int)round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            double sepiaGreen = (int)round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            double sepiaBlue = (int)round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
 
             // making the pixels sepia
             image[i][j].rgbtRed = sepiaRed;
