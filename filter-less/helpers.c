1ed7aee79b2f8b2b52de55a5bbe535c4c6b8deef
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 14:45:44 +0000

    Thu, Nov 3, 2022, 2:45 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -140,8 +140,15 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
 
     // rest of pixels
+    for (int i = 1; i < height - 1; i++)
+    {
+        for (int j = 1; j < width - 1; j++)
+        {
+            image[i][j].rgbtRed = (int)(round)((tmp[i - 1][j - 1].rgbtRed + tmp[i - 1][j].rgbtRed + tmp[i - 1][j + 1].rgbtRed + tmp[i][j - 1].rgbtRed + tmp[i][j].rgbtRed + tmp[i][j + 1].rgbtRed + tmp[i + 1][j - 1].rgbtRed + tmp[i + 1][j].rgbtRed + tmp[i + 1][j + 1].rgbtRed) / 9);
+            image[i][j].rgbtGreen = (int)(round)((tmp[i - 1][j - 1].rgbtGreen + tmp[i - 1][j].rgbtGreen + tmp[i - 1][j + 1].rgbtGreen + tmp[i][j - 1].rgbtGreen + tmp[i][j].rgbtGreen + tmp[i][j + 1].rgbtGreen + tmp[i + 1][j - 1].rgbtGreen + tmp[i + 1][j].rgbtGreen + tmp[i + 1][j + 1].rgbtGreen) / 9);
+
+        }
+    }
 
     return;
 }
-
-// TODO: avoid segfault by not stopping if pixel on border
