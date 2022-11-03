4278c4c45d640c06e999f1abf92eb512ae765875
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 10:51:35 +0000

    Thu, Nov 3, 2022, 10:51 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -51,6 +51,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
 
