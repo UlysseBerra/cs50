97171360941b32461ea07d352dd6e0501c28d748
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 14:46:23 +0000

    Thu, Nov 3, 2022, 2:46 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -146,7 +146,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         {
             image[i][j].rgbtRed = (int)(round)((tmp[i - 1][j - 1].rgbtRed + tmp[i - 1][j].rgbtRed + tmp[i - 1][j + 1].rgbtRed + tmp[i][j - 1].rgbtRed + tmp[i][j].rgbtRed + tmp[i][j + 1].rgbtRed + tmp[i + 1][j - 1].rgbtRed + tmp[i + 1][j].rgbtRed + tmp[i + 1][j + 1].rgbtRed) / 9);
             image[i][j].rgbtGreen = (int)(round)((tmp[i - 1][j - 1].rgbtGreen + tmp[i - 1][j].rgbtGreen + tmp[i - 1][j + 1].rgbtGreen + tmp[i][j - 1].rgbtGreen + tmp[i][j].rgbtGreen + tmp[i][j + 1].rgbtGreen + tmp[i + 1][j - 1].rgbtGreen + tmp[i + 1][j].rgbtGreen + tmp[i + 1][j + 1].rgbtGreen) / 9);
-
+            image[i][j].rgbtBlue = (int)(round)((tmp[i - 1][j - 1].rgbtBlue + tmp[i - 1][j].rgbtBlue + tmp[i - 1][j + 1].rgbtBlue + tmp[i][j - 1].rgbtBlue + tmp[i][j].rgbtBlue + tmp[i][j + 1].rgbtBlue + tmp[i + 1][j - 1].rgbtBlue + tmp[i + 1][j].rgbtBlue + tmp[i + 1][j + 1].rgbtBlue) / 9);
         }
     }
 
