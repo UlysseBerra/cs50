5f63ee0bab8e5b50418e74b6852c44a0fe1fcff9
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 10:46:06 +0000

    Thu, Nov 3, 2022, 10:46 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,9 +24,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            double sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
-            double sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
-            double sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+            double sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+            double sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue
+            double sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue
 
             image[i][j].rgbtRed = avg_color;
             image[i][j].rgbtGreen = avg_color;
