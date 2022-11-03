394fcda134f3055c45fa6e4470a7f3762ea5a578
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 11:14:35 +0000

    Thu, Nov 3, 2022, 11:14 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -74,5 +74,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
