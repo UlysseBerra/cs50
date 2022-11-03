617b1a642063fe1d9a145eb3f892c916783670f9
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-03 10:37:00 +0000

    Thu, Nov 3, 2022, 10:37 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -8,9 +8,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
          for (int j = 0; j < width; j++)
         {
             int avg_color = (int)((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
-            image[i][j].rgbtRed = 255;
-            image[i][j].rgbtGreen = 255;
-            image[i][j].rgbtBlue = 255;
+            image[i][j].rgbtRed = avg_color;
+            image[i][j].rgbtGreen = avg_color;
+            image[i][j].rgbtBlue = avg_color;
         }
     }
     return;
