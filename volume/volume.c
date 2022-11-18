4bff737f4e3502a57f3358a7efe7000733067e22
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-18 09:32:22 +0000

    Fri, Nov 18, 2022, 9:32 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,7 +36,9 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    
+    BYTE header[HEADER_SIZE];
+    fread(&header, HEADER_SIZE, 1, input);
+    fwrite(&header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
 
