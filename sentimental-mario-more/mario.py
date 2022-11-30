6b123931ec787cd051c2e0dada4726a31f3ec95a
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-30 12:57:19 +0000

    Wed, Nov 30, 2022, 12:57 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,13 +1,4 @@
-n = 0
-
 while True:
     n = int(input("Height: "))
-
-    if n >= 1 and n <= 8:
-        break
-
-for i in range(n):
-    print(" " * (n - i - 1), end="")
-    print("#" * (i + 1), end="")
-    print(" " * 2, end="")
-    print("#" * (i + 1), end="\n")
+    if n >= 1 and n <= 8: break
+for i in range(n): print(" " * (n - i - 1), end=""); print("#" * (i + 1), end=""); print(" " * 2, end=""); print("#" * (i + 1), end="\n")
