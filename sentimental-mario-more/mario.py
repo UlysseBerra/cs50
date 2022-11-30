36d74a3e5f18926b7010596296ef0518b8f0e0a9
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-30 07:24:06 +0000

    Wed, Nov 30, 2022, 7:24 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -8,16 +8,16 @@ def main():
 
     for a, i in enumerate(range(n)):
         for b, j in enumerate(range(n - i - 1)):
-            print(" ");
+            print(" ", end='');
 
         for c, k in enumerate(range(i - 1)):
-            print("#");
+            print("#", end='');
 
         for d, l in enumerate(range(2)):
-            print(" ");
+            print(" ", end='');
 
         for e, k in enumerate(range(i - 1)):
-            print("#");
+            print("#", end='');
 
         print("\n");
 
