0b53b78e7f8182bc76d2d285daa6767585cb3d28
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-30 12:59:21 +0000

    Wed, Nov 30, 2022, 12:59 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,4 +1,12 @@
+# prompt user for input until valid input is given
 while True:
     n = int(input("Height: "))
-    if n >= 1 and n <= 8: break
-for i in range(n): print(" " * (n - i - 1), end=""); print("#" * (i + 1), end=""); print(" " * 2, end=""); print("#" * (i + 1), end="\n")
+    if n >= 1 and n <= 8:
+        break
+
+# print pyramids
+for i in range(n):
+    print(" " * (n - i - 1), end="")
+    print("#" * (i + 1), end="")
+    print(" " * 2, end="")
+    print("#" * (i + 1))
