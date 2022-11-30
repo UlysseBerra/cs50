caefa5999937942081d76768a22f6f74d4ec3268
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-30 12:24:07 +0000

    Wed, Nov 30, 2022, 12:24 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,29 +1,9 @@
-def main():
-    # Get height from standard input
-    n = 0
-    while True:
-        n = int(input("Height: "))
-        if n >= 1 and n <= 8:
-            break
+n = 0
 
-    # Print out newlines
-    for a, i in enumerate(range(0, n, 1)):
-        # Print out indentation
-        for b, j in enumerate(range(0, n - i - 1, 1)):
-            print(" ", end="")
+while True:
+    n = int(input("Height: "))
 
-        # Print out hashes for first pyramid
-        for c, k in enumerate(range(0, i - 1, 1)):
-            print("#", end="")
+    if n >= 1 and n <= 8:
+        break
 
-        # Print out spacing in between pyramids
-        for d, l in enumerate(range(0, 2, 1)):
-            print(" ", end="")
-
-        # Print out hashes for second pyramid
-        for e, k in enumerate(range(0, i - 1, 1)):
-            print("#", end="")
-
-        print("\n")
-
-main()
+for 
