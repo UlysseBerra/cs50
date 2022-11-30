5f93b6b7a7730d2670c0643795343dda31005142
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-30 14:20:02 +0000

    Wed, Nov 30, 2022, 2:20 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,42 +1,43 @@
 from cs50 import *
 
 # finds the number of letters
+
+
 def get_letters(text_in):
     letters = 0
-    i = 0
 
-    while text_in[i] != '\0':
-        if text_in[i].isalpha():
+    for i in text_in:
+        if i.isalpha():
             letters += 1
-        i += 1
 
     return letters
 
 # finds the number of words
+
+
 def get_words(text_in):
     words = 0
-    i = 0
 
-    while text_in[i] != '\0':
-        if text_in[i] == ' ':
+    for i in text_in:
+        if i == ' ':
             words += 1
-        i += 1
 
     words += 1
     return words
 
 # finds the number of sentences
+
+
 def get_sentences(text_in):
     sentences = 0
-    c = 0
 
-    while text_in[c] != '\0':
-        isSentence = text_in[c] == '.' or text_in[c] == '!' or text_in[c] == '?'
+    for c in text_in:
+        isSentence = c == '.' or c == '!' or c == '?'
         if isSentence:
             sentences += 1
-        c += 1
 
     return sentences
+
 
 def main():
     # gets input from user
@@ -52,11 +53,13 @@ def main():
     S = (float(sentences / words)) * 100
     index = (0.0588 * L) - (0.296 * S) - 15.8
 
+    # prints index
     if index < 1:
         print("Before Grade 1")
     elif index >= 16:
         print("Grade 16+")
     else:
         print(f"Grade {int(round(index))}")
+
 
 main()
