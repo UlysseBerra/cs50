81bb149b3288b48537b914e3b88e6851a440c8cf
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-11-30 13:17:33 +0000

    Wed, Nov 30, 2022, 1:17 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,31 +1,5 @@
 import cs50
 
-def main():
-    # Ask how many cents the customer is owed
-    cents = get_cents()
-
-    # Calculate the number of quarters to give the customer
-    quarters = calculate_quarters(cents)
-    cents = cents - quarters * 25
-
-    # Calculate the number of dimes to give the customer
-    dimes = calculate_dimes(cents)
-    cents = cents - dimes * 10
-
-    # Calculate the number of nickels to give the customer
-    nickels = calculate_nickels(cents)
-    cents = cents - nickels * 5
-
-    # Calculate the number of pennies to give the customer
-    pennies = calculate_pennies(cents)
-    cents = cents - pennies * 1
-
-    # Sum coins
-    coins = quarters + dimes + nickels + pennies
-
-    # Print total number of coins to give the customer
-    print(coins)
-
 # Get the amount of cents owed
 def get_cents():
     cents = 0
@@ -77,5 +51,31 @@ def calculate_pennies(cents):
         pennies += 1
 
     return pennies
+
+def main():
+    # Ask how many cents the customer is owed
+    cents = get_cents()
+
+    # Calculate the number of quarters to give the customer
+    quarters = calculate_quarters(cents)
+    cents = cents - quarters * 25
+
+    # Calculate the number of dimes to give the customer
+    dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
+
+    # Calculate the number of nickels to give the customer
+    nickels = calculate_nickels(cents)
+    cents = cents - nickels * 5
+
+    # Calculate the number of pennies to give the customer
+    pennies = calculate_pennies(cents)
+    cents = cents - pennies * 1
+
+    # Sum coins
+    coins = quarters + dimes + nickels + pennies
+
+    # Print total number of coins to give the customer
+    print(coins)
 
 main()
