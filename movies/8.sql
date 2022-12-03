d80f6f5ae7e7a601fed382b2602156bb1c36b432
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-12-03 18:25:45 +0000

    Sat, Dec 3, 2022, 6:25 PM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON people.id = person_id JOIN movies ON movie_id = movies.id;
+SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE movies.title = "Toy Story";
