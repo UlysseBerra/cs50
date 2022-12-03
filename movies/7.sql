1a83475be6cf8edb44fa9f4269e0e73d68113cea
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-12-03 18:24:40 +0000

    Sat, Dec 3, 2022, 6:24 PM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT movies.title, ratings.rating FROM movies, ratings WHERE movies.year = "2010" ORDER BY ratings.rating;
+SELECT title, rating FROM movies JOIN ratings ON id = movie_id WHERE year = "2010" ORDER BY rating DESC, title ASC;
