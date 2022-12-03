af8e49af4baf4e9970dc4a80edc237958efd3cb4
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-12-03 07:40:48 +0000

    Sat, Dec 3, 2022, 7:40 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -4,10 +4,12 @@ import sys
 
 def main():
 
-    # TODO: Check for command-line usage
+    if len(sys.argv) != 3:
+        print("Wrong argument usage.")
+        return 1
 
     # TODO: Read database file into a variable
-    
+
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
@@ -43,11 +45,11 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
