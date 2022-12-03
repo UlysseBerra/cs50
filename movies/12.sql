4fde443407562f5b9e7c84de49ed04bdba40115a
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-12-03 19:03:57 +0000

    Sat, Dec 3, 2022, 7:03 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies WHERE id IN (SELECT stars.movie_id FROM stars WHERE stars.person_id = (SELECT name FROM people WHERE id = 'Johnny Depp'));
+SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Johnny Depp" AND movies.title IN( SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE people.name = "Helena Bonham Carter");
