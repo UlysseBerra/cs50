78fce094b2d9b8644e46d73941a2c691de486060
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-12-03 11:48:22 +0000

    Sat, Dec 3, 2022, 11:48 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -15,7 +15,8 @@ def main():
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = []
-    # TODO: Read teams into memory from file
+    with open()
+
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
