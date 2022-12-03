a8dc42367b49c6f59c1bcc9177d7bef305ef4012
UlysseBerra <75935300+UlysseBerra@users.noreply.github.com>
2022-12-03 18:52:09 +0000

    Sat, Dec 3, 2022, 6:52 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,7 +1,1 @@
-SELECT title FROM movies
-WHERE id IN (
-            SELECT stars.movie_id FROM stars WHERE stars.person_id IN (
-                                                                     SELECT name FROM people WHERE id = 'Johnny Depp'
-                                                                     )
-            )
-;
+SELECT title FROM movies WHERE id IN (SELECT stars.movie_id FROM stars WHERE stars.person_id = (SELECT name FROM people WHERE id = 'Johnny Depp'));
